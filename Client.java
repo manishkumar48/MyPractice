8ada33dae8a9972d065443b22c6317bcba3ab9be
manishkumar48 <53835592+manishkumar48@users.noreply.github.com>
2019-08-07 00:28:01 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Client {
+
+	public static void main(String[] args) {
+			System.out.println("Converted word is==>>>    " + NumberToWord.convertToWord(args[0]));
+
+	}
+
+}
---
+++ NumberToWordTest.java
@@ -0,0 +1,15 @@
+package com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberToWordTest {
+
+	@Test
+	public void testConvert() {
+		NumberToWord.convert(46536434);
+		assertTrue(true);
+	}
+
+}
